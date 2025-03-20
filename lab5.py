37e1784ff2b41349eef30dc282daeb6ff04ea05d
Merve Topal <153943064+mervtpl@users.noreply.github.com>
2025-03-20 13:41:08 +0300

    lab5.py

---
+++ lab5.py
@@ -0,0 +1,45 @@
+import random
+import string
+dict={}
+dictKey=set()
+dictValue=set()
+print("Choose 10 characters (lowercase only) and assign 5 replacement options each:")
+for _ in range(2):
+    print("enter a lowercase character:")
+    key=input()
+    dict[key]=set()
+    for _ in range(1):
+        print("enter a replacement for  " + key)
+        value=input()
+        dict[key].add(value)
+
+    
+
+
+print(dict)
+passwords = []
+weakPasswords = []
+strongPasswords = []
+for _ in range(5):
+    password = ''.join(random.choices(string.ascii_lowercase, k=15))
+    passwords.append(password)
+print(passwords)
+count=0;
+for password in passwords:
+    for key in dict:
+            password = password.replace(key, random.choice(list(dict[key])))
+            for x in password:
+                if x==key:
+                    count+=1
+
+            if(count<5):
+                 weakPasswords.append(password)
+            else:
+                strongPasswords.append(password)
+
+
+print(weakPasswords)
+print(strongPasswords)
+
+
+
